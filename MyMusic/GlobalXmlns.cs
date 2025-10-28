e7a14ee5c5ceb2ecffb258ccf9d2e072b918bba8
norbertc5 <norbert07c@gmail.com>
2025-10-28 09:01:33 +0100

    added project

---
+++ MyMusic/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MyMusic")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MyMusic.Pages")]
---
+++ MyMusic/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MyMusic
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
