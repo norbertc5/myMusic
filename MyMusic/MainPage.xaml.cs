40b6446a053afc770fe2a67b30c56baf208e0b82
norbertc5 <norbert07c@gmail.com>
2025-10-28 10:22:47 +0100

    some ui

--- MyMusic/MainPage.xaml.cs
+++ MyMusic/MainPage.xaml.cs
@@ -2,23 +2,13 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
+        List<string> songs = new List<string>();
         public MainPage()
         {
             InitializeComponent();
-        }
-
-        private void OnCounterClicked(object? sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            songs.Add("Sample song");
+            songs.Add("Sample song");
+            playlist.ItemsSource = songs;
         }
     }
 }
