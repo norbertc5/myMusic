38e29fd1dfa2b904d36a0afc204e7676f4e53597
Student <Student@10ffe0d7fd2d>
2025-11-17 10:18:25 +0100

    song clicking

--- MyMusic/MainPage.xaml.cs
+++ MyMusic/MainPage.xaml.cs
@@ -3,12 +3,35 @@
     public partial class MainPage : ContentPage
     {
         List<string> songs = new List<string>();
+        List<string> likedSongs = new List<string>();
+        public Command<string> PlaySongCommand { get; set; }
+
+
         public MainPage()
         {
             InitializeComponent();
-            songs.Add("Sample song");
-            songs.Add("Sample song");
+            songs.Add("s1");
+            songs.Add("s2");
             playlist.ItemsSource = songs;
+            PlaySongCommand = new Command<string>(OnSongClicked);
+            BindingContext = this;
+
+        }
+
+        private void OnSongClicked(string song)
+        {
+            // TU WIESZ KTÓRA PIOSENKA ZOSTAŁA KLIKNIĘTA
+            DisplayAlert("Kliknięto", $"Kliknięto: {song}", "OK");
+        }
+
+        private void Like_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Play_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
